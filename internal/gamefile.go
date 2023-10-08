d409923cc63c9056f0c8e78cf4b6ecc9f99ea713
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-08 08:42:48 +0100

    Working on the game window

--- internal/gamefile.go
+++ internal/gamefile.go
@@ -1,1 +1,25 @@
 package internal
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	screenWidth  = 1200
+	screenHeight = 950
+)
+
+type Game struct{}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Testing unit")
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"gogameV2/internal"
+	"log"
 
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func main() {
+	g := internal.NewGame()
+	ebiten.SetWindowTitle("Testing")
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
 }
